6a82c6d4373263205e81f800f87e36a58c70677e
n-clarke <56034641+n-clarke@users.noreply.github.com>
2021-02-14 17:36:44 +0000

    Update MultithreadingChallenge.java

--- src/main/java/com/wandisco/challenge/two/componentised/MultithreadingChallenge.java
+++ src/main/java/com/wandisco/challenge/two/componentised/MultithreadingChallenge.java
@@ -6,14 +6,7 @@ import static com.wandisco.challenge.two.componentised.service.Problem.problem2;
 class MultithreadingChallenge {
 
     public static void main(final String[] args) throws InterruptedException {
-        /** You can use jconsole, jvisualvm or jps to get the process id of your program and then run jstack <process-id> to get the thread stack dump :
-        * https://javajee.com/lock-ordering-deadlocks-in-java-with-example
-        **/
-        
-        /** Synchronized issue
-        * reason: https://docs.oracle.com/javase/tutorial/essential/concurrency/memconsist.html
-        * solution: https://docs.oracle.com/javase/tutorial/essential/concurrency/syncmeth.html
-        **/
+        // Synchronisation issue
         problem1();
 
         // Deadlock Issue
